dd372926cf320a36222b43553191403db7bcd749
fhernanb <fhernanb@unal.edu.co>
2018-11-19 22:41:39 -0500

    mejorado

---
+++ docs/expectativa_vida.R
@@ -0,0 +1,26 @@
+library(gganimate)
+library(gapminder)
+data("gapminder")
+
+ggplot(gapminder, aes(gdpPercap, lifeExp, size=pop, colour=country)) +
+  geom_point(alpha=0.3, show.legend=FALSE) +
+  scale_colour_manual(values = country_colors) +
+  scale_size(range = c(2, 12)) +
+  geom_text(aes(label=ifelse(country=='Colombia',
+                             as.character(country), '')), 
+            hjust=1, vjust=0, color='black', 
+            show.legend=FALSE, size=4) +
+  scale_x_log10() +
+  facet_wrap(~continent) 
+
+
++
+  # Here comes the gganimate specific bits
+  labs(title = 'Año: {frame_time}', 
+       x = 'Ingreso per cápita', 
+       y = 'Expectativa de vida (años)') +
+  transition_time(year) +
+  ease_aes('linear') 
+
+
+
