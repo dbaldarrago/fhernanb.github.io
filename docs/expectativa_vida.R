30d9d04fbc0f0afca83244eef823e3ec9cd752cb
fhernanb <fhernanb@unal.edu.co>
2018-11-19 23:07:23 -0500

    mejoras

--- docs/expectativa_vida.R
+++ docs/expectativa_vida.R
@@ -5,12 +5,12 @@ data("gapminder")
 ggplot(gapminder, aes(gdpPercap, lifeExp, size=pop, colour=country)) +
   geom_point(alpha=0.7, show.legend=FALSE) +
   geom_point(data=gapminder[gapminder$country=='Colombia', ], 
-             aes(x=gdpPercap, y=lifeExp), colour="yellow", size=5) +
+             aes(x=gdpPercap, y=lifeExp), colour="royalblue3", size=5) +
   scale_colour_manual(values = country_colors) +
   scale_size(range = c(2, 12)) +
   geom_text(aes(label=ifelse(country=='Colombia',
                              as.character(country), '')), 
-            hjust=1, vjust=0, color='black', 
+            hjust=1, vjust=0, color='royalblue3', 
             show.legend=FALSE, size=4) +
   scale_x_log10() +
   facet_wrap(~continent) +
