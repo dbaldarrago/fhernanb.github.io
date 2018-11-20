aabcf1ee490bc306ede6278e9bedbd5c9f92d656
fhernanb <fhernanb@unal.edu.co>
2018-11-19 22:55:43 -0500

    archivo para crear el grafico inicial

--- docs/expectativa_vida.R
+++ docs/expectativa_vida.R
@@ -3,7 +3,9 @@ library(gapminder)
 data("gapminder")
 
 ggplot(gapminder, aes(gdpPercap, lifeExp, size=pop, colour=country)) +
-  geom_point(alpha=0.3, show.legend=FALSE) +
+  geom_point(alpha=0.7, show.legend=FALSE) +
+  geom_point(data=gapminder[gapminder$country=='Colombia', ], 
+             aes(x=gdpPercap, y=lifeExp), colour="yellow", size=5) +
   scale_colour_manual(values = country_colors) +
   scale_size(range = c(2, 12)) +
   geom_text(aes(label=ifelse(country=='Colombia',
@@ -11,10 +13,7 @@ ggplot(gapminder, aes(gdpPercap, lifeExp, size=pop, colour=country)) +
             hjust=1, vjust=0, color='black', 
             show.legend=FALSE, size=4) +
   scale_x_log10() +
-  facet_wrap(~continent) 
-
-
-+
+  facet_wrap(~continent) +
   # Here comes the gganimate specific bits
   labs(title = 'Año: {frame_time}', 
        x = 'Ingreso per cápita', 
